f0a20fe18f8032c2aa747c4fd17fe5b21f20a8dc
WuYuanHeng <wuyuanheng@WuYuanHeng-MacBook-Pro.local>
2020-02-03 23:25:11 +0800

    Initial commit

---
+++ src/main/java/com/ahoneybee/bolg/BolgApplication.java
@@ -0,0 +1,13 @@
+package com.ahoneybee.bolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolgApplication.class, args);
+    }
+
+}
